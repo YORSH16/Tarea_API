be9b71a27ca1c264985d253f770a4748105e69c3
YORSH16 <109051056+YORSH16@users.noreply.github.com>
2024-03-08 00:32:20 -0600

    Registro de usuario

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from fastapi import FastAPI
+
+import uuid
+
+app = FastAPI(
+    title="APIs en clase de Mlops 5",
+    version="0.0.1"
+)
+
+@app.post("/api/v1/users/")
+async def create_user(username: str, mail: str, password: str):
+    return {
+        "username": username,
+        "mail": mail,
+        "password": password,
+        "message": "El usuario se creo correctamente",
+        "status_code": 201
+    }
 
 
 
@@ -16,3 +34,5 @@
 
 
 
+
+#%%
